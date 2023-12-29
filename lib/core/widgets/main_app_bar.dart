afba54b4843f8da1d33f22d04875ac232a2c4bc1
20120454 <dat13102k2@gmail.com>
2023-12-29 22:36:46 +0700

    new chat button on app bar

--- lib/core/widgets/main_app_bar.dart
+++ lib/core/widgets/main_app_bar.dart
@@ -1,20 +1,36 @@
+import 'package:boilerplate/di/service_locator.dart';
+import 'package:boilerplate/presentation/chat_screen/store/chat_store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainAppBar extends StatelessWidget {
+class MainAppBar extends StatefulWidget {
   const MainAppBar({super.key});
 
+  @override
+  State<MainAppBar> createState() => _MainAppBarState();
+}
+
+class _MainAppBarState extends State<MainAppBar> {
+  ChatStore _chatStore = getIt<ChatStore>();
   @override
   Widget build(BuildContext context) {
     return AppBar(
       title: Text('ChatGPT'),
       actions: [
-        IconButton(
-          icon: FaIcon(FontAwesomeIcons.penToSquare),
-          splashRadius: 20,
-          iconSize: 20,
-          onPressed: () {},
-        ),
+        Observer(builder: (context) {
+          return IconButton(
+            icon: FaIcon(FontAwesomeIcons.penToSquare),
+            splashRadius: 20,
+            iconSize: 20,
+            disabledColor: Colors.grey,
+            onPressed: _chatStore.id > 0
+                ? () {
+                    _chatStore.setChatThreadId(-1);
+                  }
+                : null,
+          );
+        }),
         IconButton(
           icon: FaIcon(FontAwesomeIcons.ellipsisVertical),
           splashRadius: 20,
